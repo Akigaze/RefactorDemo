13d8d2280895532dfa27bc5f11ca832110e8dde7
quinnHuang <quinn.huang@oocl.com>
2018-08-24 14:47:47 +0800

    [Quinn] add test for method statement if rent more than one movies

--- src/test/java/rentalstore/CustomerTest.java
+++ src/test/java/rentalstore/CustomerTest.java
@@ -88,4 +88,38 @@ public class CustomerTest {
         //then
         Assert.assertEquals(exceptedStatement,statement);
     }
+
+    @Test
+    public void should_print_statement_when_rent_a_children_movie_for_4_day_and_2_regular_movie_for_2_days_and_2_new_release_movies_for_2_days(){
+        //given
+        Movie regularMovie1 =new Movie("A Journey to the West",Movie.REGULAR);
+        Movie regularMovie2 =new Movie("Avatar",Movie.REGULAR);
+        Movie newReleaseMovie1 =new Movie("Death on the Nile",Movie.NEW_RELEASE);
+        Movie newReleaseMovie2 =new Movie("GG Bond",Movie.NEW_RELEASE);
+        Movie childrenMovie =new Movie("Peppa Pig",Movie.CHILDRENS);
+        Customer renter = new Customer("Smith");
+        Rental rental1 = new Rental(childrenMovie,4);
+        Rental rental2 = new Rental(regularMovie1,2);
+        Rental rental3 = new Rental(regularMovie2,2);
+        Rental rental4 = new Rental(newReleaseMovie1,2);
+        Rental rental5 = new Rental(newReleaseMovie2,2);
+        renter.addRental(rental1);
+        renter.addRental(rental2);
+        renter.addRental(rental3);
+        renter.addRental(rental4);
+        renter.addRental(rental5);
+
+        String exceptedStatement = "Rental Record for Smith\n"
+                +"\tPeppa Pig: 3.0\n"
+                +"\tA Journey to the West: 2.0\n"
+                +"\tAvatar: 2.0\n"
+                +"\tDeath on the Nile: 6.0\n"
+                +"\tGG Bond: 6.0\n"
+                +"You owe 19.0\n"
+                +"On this rental you earned 7 frequent renter points";
+        //when
+        String statement = renter.statement();
+        //then
+        Assert.assertEquals(exceptedStatement,statement);
+    }
 }
